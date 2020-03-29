3d92273f0b49dd49e36c34d494bed95de5adc3e7
mdjacksparrow <mdjack01122000@gmail.com>
2020-03-29 15:45:56 +0530

    del unwanted folder and add user connected and disconnected msg to broadcast

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,18 @@
 // Initialize the socket.io-client server for bi-directional communicaiton
 var socket = io();
+
 $(document).ready(function() {
+
+  // show user when connected to the group 
+  socket.emit('init', 'User is connected!');
+
+  socket.on('init', info => {
+    $('.chat-messages').append(
+      '<div class = "message">' +
+        `<p class="notify">${info}</p>`
+    );
+  });
+
   $('form').submit(e => {
     e.preventDefault();
     socket.emit('brodcast', $('#msg').val());
@@ -8,22 +20,36 @@ $(document).ready(function() {
     return false;
   });
 
-   socket.on('restore', msg => {
+  socket.on('restore', msg => {
     console.log(msg);
-    msg.forEach(localmsg => {
-        $('.chat-messages').append('<div class = "message">'
-        + '<p class="meta">Brad <span> 9:12pm </span> </p>'
-        + '<p class="text">'
-        + `${localmsg} </p>`
-        ); 
-    });
-})
 
-  socket.on('brod', msg => {
-    $('.chat-messages').append('<div class = "message">'
-    + '<p class="meta">Brad <span> 9:12pm </span> </p>'
-    + '<p class="text">'
-    + `${msg} </p>`
-    ); 
+    msg.forEach(restore => {
+      $('.chat-messages').append(
+        '<div class = "message">' +
+          '<p class="text">' +
+          `<p class="meta">Username<span> 9:12pm </span> </p>` +
+          `${restore} </p>`
+      );
+    });
   });
+
+  // Add meto info above the p Element
+  // + '<p class="meta">Brad <span> 9:12pm </span> </p>'
+
+  socket.on('brodcast', msg => {
+    $('.chat-messages').append(
+      '<div class = "message">' +
+        `<p class="meta">username <span> 9:12pm </span> </p>` +
+        '<p class="text">' +
+        `${msg} </p>`
+    );
+  });
+});
+
+// Show msg when the user is disconnected 
+socket.on('end', info => {
+  $('.chat-messages').append(
+    '<div class = "message">' +
+      `<p class="notify">${info}</p>`
+  );
 });
--- server.js
+++ server.js
@@ -1,27 +1,39 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const ejs = require('ejs');
+const bodyParser = require('body-parser')
+// const url = require('url');
+
 
 // To store excited messages instead of using DB 
-var localMsg = ['Welcome from yap-chat!'];
+var localMsg = [];
 
 // Set public as a local folder to fetch our local html and css files 
 app.use(require('express').static(__dirname + '/public'));
 // Setup view engine template 
 app.set('view engine', 'ejs');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 app.get('/chat-page', (req, res) => {
+
+  // var url_parts = url.parse(req.url, true);
+  // var query = url_parts.query;
+
+  // users.push(query.username);
+
   res.render('chat');
 });
 
 // Initialize the socket connection 
 io.on('connection', socket => {
-  console.log('A user connected!');
+  socket.on('init', msg => {
+    io.emit('init', msg);
+  });
 
   socket.emit('restore', localMsg);
 
@@ -33,6 +45,7 @@ io.on('connection', socket => {
 
   socket.on('disconnect', () => {
     console.log('A user is disconnected!');
+    io.emit('end','A user is disconnected!');
   });
 });
 
