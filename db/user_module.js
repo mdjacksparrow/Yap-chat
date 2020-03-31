2b98407cd72bfc7fe46cc049853e60303a5357eb
mdjacksparrow <mdjack01122000@gmail.com>
2020-03-31 19:20:24 +0530

    bug fixed

--- db/user_module.js
+++ db/user_module.js
@@ -1,20 +1,19 @@
 const db = require('./dbConnection');
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Initialize the db connection 
+// Initialize the db connection
 db.connect();
 console.log('Db connected !');
 
 const userMsg = new Schema({
   username: {
+    type: String
+    // required: [true]
+  },    
+  msg: {
     type: String,
     required: [true]
-  },
-  msg:{
-    type : String,
-    required : [true]
   }
 });
-
-exports.User = new mongoose.model("message-storage", userMsg);
+exports.User = new mongoose.model('message-storage', userMsg);
