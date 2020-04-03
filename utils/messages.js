a669d0af775487ce3ee4b650922d2f2756a57f69
mdjacksparrow <mdjack01122000@gmail.com>
2020-04-03 16:44:35 +0530

    same remove getTime() in message.js

--- utils/messages.js
+++ utils/messages.js
@@ -6,24 +6,3 @@ exports.formatMsg = (username, message, time) => {
     time
   };
 }
-
-exports.getTime = () => {
-  // For reference 
-  // Create instance of Date and get the notation 
-  // const notation = new Date().toLocaleString().split(' ')[2];
-
-  //split the time zone
-  // const timearr = new Date().toLocaleString().split(' ')[1].split(':');
-
-  return `${
-    new Date()
-      .toLocaleString()
-      .split(' ')[1]
-      .split(':')[0]
-  }:${
-    new Date()
-      .toLocaleString()
-      .split(' ')[1]
-      .split(':')[1]
-  } ${new Date().toLocaleString().split(' ')[2]}`;
-}
