4696e4d20a636af6f14a9402a8041eadab878aaf
mdjacksparrow <mdjack01122000@gmail.com>
2020-04-03 16:44:05 +0530

    remove getTime() function

--- public/js/main.js
+++ public/js/main.js
@@ -126,26 +126,6 @@ $(document).ready(function() {
   });
 });
 
-function getTime() {
-  // For reference
-  // Create instance of Date and get the notation
-  // const notation = new Date().toLocaleString().split(' ')[2];
-
-  //split the time zone
-  // const timearr = new Date().toLocaleString().split(' ')[1].split(':');
-
-  return `${
-    new Date()
-      .toLocaleString()
-      .split(' ')[1]
-      .split(':')[0]
-  }:${
-    new Date()
-      .toLocaleString()
-      .split(' ')[1]
-      .split(':')[1]
-  } ${new Date().toLocaleString().split(' ')[2]}`;
-}
 
 // Return object
 function putUserMsg(msg, time) {
